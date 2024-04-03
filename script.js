4fe916b68a7bf2120b1b1351071f1ea761105aef
Antônio Carlos <antonioocarlos@proton.me>
2024-04-03 17:26:09 -0300

    refactor: refactored 'clone' function

--- script.js
+++ script.js
@@ -244,10 +244,15 @@ function isNumberKey(e) {
 }
 
 function clone(obj) {
-  if (null == obj || "object" != typeof obj) return obj;
-  var copy = obj.constructor();
-  for (var attr in obj) {
-      if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+  if (obj == null || typeof obj != "object")
+    return obj;
+   
+  let copy = obj.constructor();
+
+  for (let attr in obj) {
+      if (obj.hasOwnProperty(attr))
+        copy[attr] = obj[attr];
   }
+  
   return copy;
 }
